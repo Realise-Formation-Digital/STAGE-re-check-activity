3a5c122221a9149ccdda80f09b0cf1eb2c37c33d
Kilgor1213 <tristan.tsst@gmail.com>
2021-04-07 16:39:17 +0200

    WIP Hello.vue

--- imports/ui/components/Hello.vue
+++ imports/ui/components/Hello.vue
@@ -1,28 +1,27 @@
 <template>
   <div>
-    <Navbar/>
-    <FormBuilding/>
+    <Navbar />
+    <FormBuilding />
   </div>
 </template>
 
 <script>
-import Navbar from './Navbar.vue'
-import FormBuilding from './FormBuilding.vue'
+import Navbar from "./Navbar.vue";
+import FormBuilding from "./FormBuilding.vue";
 
 export default {
   data() {
     return {
-   counter: 0,
-    }
+      counter: 0,
+    };
   },
 
-  components: { Navbar, FormBuilding
-  }
-}
+  components: { Navbar, FormBuilding },
+};
 </script>
 
 <style scoped>
-  p {
-    font-family: serif;
-  }
+p {
+  font-family: serif;
+}
 </style>
