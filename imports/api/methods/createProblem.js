8aab3e920abc54f21d6f2d78b14bc8f75a5a85c7
aoumar <aoumar.lahouari@gmail.com>
2021-04-13 12:00:39 +0200

    mise à jour
    mise à jour edit/delete
    de createProblem

--- imports/api/methods/createProblem.js
+++ imports/api/methods/createProblem.js
@@ -15,4 +15,25 @@ Meteor.methods({
   'deleteproblem'(_id){
     Problems.remove(_id)
  },
+ /**
+   * Edit of a problem
+   * @param {*} id - Id of the problem that we want to modify
+   * @param {*} title -  title of the problem 
+   * @param {*} description - Description of problem
+   */
+
+  'editProblem'(id, title, description) {
+
+    // First we have to check if the fields are inserted in the right way 
+    check(title, String);
+    check(description, String);
+    check(problemId, String);
+
+    // we have to update the database
+    Problem.update({ "_id": id }, {
+      'title': title,
+      'description': description,
+      'problemId': problemId
+    })
+  }
 });
