acc80554e26e16623745745048d7d6f42bd9f013
Kilgor1213 <tristan.tsst@gmail.com>
2021-04-15 11:09:59 +0200

    Form login added on page Home
    Ajout form dans la page index

--- imports/ui/components/Home.vue
+++ imports/ui/components/Home.vue
@@ -1,6 +1,29 @@
 <template>
+  <div class="LoginForm">
+<v-app>
   <div>
-
+		<v-content>
+        <div class="LoginForm">
+				<v-layout row wrap>
+					<v-flex xs12 sm8 id="sign-up-form-wrapper" class="active">
+						<v-layout column align-center justify-center pa-3 mt-5>
+							<v-flex xs12 mb-3 mt-5>
+								<div class="login-wrapper text-xs-center">
+									<div class="display-1 font-weight-black">Se connecter</div>
+								</div>
+							</v-flex>
+							<v-flex xs12 class="form-wrapper">
+								<v-text-field box full-width single-line label="Username" background-color="#f4f8f7" color="grey darken-2" prepend-inner-icon="mdi-account"></v-text-field>
+								<v-text-field v-model="password" :append-icon="show1 ? 'mdi-eye-outline' : 'mdi-eye-off-outline'" :type="show1 ? 'text' : 'password'" box full-width single-line label="Identifiant/Password" background-color="#f4f8f7" color="grey darken-2" prepend-inner-icon="mdi-lock-outline" @click:append="show1 = !show1"></v-text-field>
+							</v-flex>
+							<v-btn round large dark ripple color="teal" id="sign-up" @click="show = !show">Sign up</v-btn>
+						</v-layout>
+					</v-flex>
+				</v-layout>
+        </div>
+		</v-content>
+  </div>
+	</v-app>
     <router-view></router-view>
 
   </div>
@@ -16,3 +39,14 @@ export default {
   },
 };
 </script>
+
+<style>
+.LoginForm {
+
+  margin-left:auto;
+  margin-right:auto;
+  padding-left: 150px;
+  padding-bottom: auto;
+}
+
+</style>
