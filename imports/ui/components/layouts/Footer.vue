c9657355e25fab82d848c68eb0b4ea1615494e6f
Marco Tribuzio <marco.tribuz@gmail.com>
2021-11-08 12:12:26 +0100

    Updated Footer

--- imports/ui/components/layouts/Footer.vue
+++ imports/ui/components/layouts/Footer.vue
@@ -1,19 +1,6 @@
 <template>
   <v-footer color="black lighten-1" fixed>
     <v-row justify="center" no-gutters>
-      <v-col class="black lighten-2 text-center white--text" cols="12">
-        <v-btn
-          v-for="link in links"
-          :key="link"
-          color="white"
-          text
-          rounded
-          class="my-2"
-        >
-          {{ link }}
-        </v-btn>
-        {{ new Date().getFullYear() }}
-      </v-col>
       <div class="subheading">
         <v-col class="black lighten-2" cols="12">
           <v-img src="img/imageLogoRealise.png" max-height="60" max-width="120" />
